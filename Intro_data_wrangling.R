047491db3c209f7080dae81c6f109acd066ee03a
James Dayhuff <home@Jamess-MacBook-Pro.local>
2016-12-08 09:24:35 -0700

    added data wrangling file

---
+++ Intro_data_wrangling.R
@@ -0,0 +1,13 @@
+## This tutorial describes how to manipulate data with data.table R package. 
+## It is considered as the fastest R package for data wrangling. 
+## Analysts generally call R programming not compatible with big datasets ( > 10 GB) 
+### as it is not memory efficient and loads everything into RAM. 
+## To change their perception, 'data.table' package comes into play. 
+## This package was designed to be concise and painless. 
+## There are many benchmarks done in the past to compare dplyr vs data.table. 
+## In every benchmark, data.table wins. 
+## The efficiency of this package was also compared with python' package (panda). 
+## And data.table wins. 
+## In CRAN, there are more than 200 packages that are dependent on data.table 
+### which makes it listed in the top 5 R's package.
+
