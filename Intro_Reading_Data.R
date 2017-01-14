8521332a4722e78582cf16cf4992da417981a19e
James <dayhuffj@gmail.com>
2017-01-14 13:18:39 -0700

    additions to reading in data

--- Intro_Reading_Data.R
+++ Intro_Reading_Data.R
@@ -20,6 +20,62 @@ getwd()
 ## txt, csv, excel, json, xml, html tables, relational DB, non-relational DB, and more!
 ## some of these require unique packages to read the data in
 
+?read.table
+
+#### TXT FILE ####
 my_file_name = "sample_text_file.txt"
 my_txt_data = read.table(my_file_name)
+# or
+my_txt_data = read.table("sample_text_file.txt")
+
+#### CSV FILE ####
+my_csv_data = read.csv("sampledata.csv")
+my_csv_data1 = read.table("sampledata.csv")
+
+View(my_csv_data)
+View(my_csv_data1)
+
+my_csv_data2 = read.table("sampledata.csv", sep=",")
+View(my_csv_data2)
+
+
+#### EXCEL FILE ####
+library(XLConnect)
+## if it says there is no package, you'll have to install in the package from scratch, which isn't hard
+?XLConnect
+my_xl_data = loadWorkbook("excel_data_sample.xlsx")
+View(my_xl_data)
+# what's wrong???
+wb = loadWorkbook("excel_data_sample.xlsx")
+my_xl_data = readWorksheet(wb, sheet=1)
+
+# shortcut
+my_xl_data = readWorksheetFromFile("excel_data_sample.xlsx",sheet = 1)
+
+#### JSON FILE ####
+library(rjson)
+?rjson
+jsondata = fromJSON(file="my_json_file")
+
+#### XML FILE ####
+library(XML)
+??XML
+
+#### HTML TABLES ####
+
+?url
+?readHTMLTable
+my_url = "http://basketball.realgm.com/nba/stats"
+nba_data = readHTMLTable(my_url, which=1)
+head(nba_data)
+str(nba_data)
+nba_data = readHTMLTable(my_url, which=1, stringsAsFactors=FALSE,colClasses=c("numeric","character","character",rep("numeric",90)))
+str(nba_data)
+head(nba_data)
+
+
+
+
+
+
 
