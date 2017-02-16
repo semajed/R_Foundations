c40734ccc72239d3d674ccbd94e20642f421882a
James <dayhuffj@gmail.com>
2017-02-16 16:06:15 -0700

    finished EDA initial steps. Very  basic.

--- EDA_Initial_Steps.R
+++ EDA_Initial_Steps.R
@@ -6,6 +6,7 @@ bike_buyers = read.csv("assets/bike_buyers.csv")
 str(bike_buyers)
 summary(bike_buyers)
 class(bike_buyers)
+
 View(bike_buyers)
 head(bike_buyers)
 
@@ -14,7 +15,7 @@ head(bike_buyers)
 # if we wanted to avoid converting strings to factors, how would we do that?
 
 
-
+######################### NOTES #########################
 #### Examine univariate data ####
 ## univariate data = samples of one variable
 ## univariate data analysis isn't concerned with the 'why'. It is just to describe the data as is.
@@ -26,6 +27,16 @@ head(bike_buyers)
 ## two key things to discover in EDA: central tendency, spread
 ### central tendency: what is a common value? what's the value around which the data is centered?
 ### spread: how variable are the data points?
+
+## Types of graphs to use:
+# box plot
+# histogram
+# density plot
+# pie graph
+
+
+######################### NOTES #########################
+
 
 ## INCOME: discrete or continouous? continuous.
 
@@ -56,20 +67,45 @@ pie(table(bike_buyers$Marital.Status), main="Married vs Single Buyers")
 
 
 # CLASS WORK: how far do these people commute?
+# Did more people buy bikes or not buy bikes?
 
 summary(bike_buyers$Commute.Distance)
 plot(bike_buyers$Commute.Distance)
 
-commute_table = table(bike_buyers$Commute.Distance)
-plot(density(commute_table), main="Commute Distance Spread")
+
+summary(bike_buyers$Purchased.Bike)
+plot(bike_buyers$Purchased.Bike)
+
+# you might ask yourself, what kind of people bought bikes?
 
 
-
+######################### NOTES #########################
 #### Examine multivariate data ####
+### Three broad classes of multivariate analysis:
+# 1) The relationship between one categorical variable and one continuous variable
+# 2) The relationship between two categorical variables
+# 3) The relationship between two continuous variables
+
+## You're wanting to find relationships between variables
+## You aren't worried about the WHY right now, just the WHAT
+
+## Types of graphs to use:
+# scatter plot
+
+######################### NOTES #########################
+
+####################################### categorical & continuous 
+
+## compare education to income
+?by
+?boxplot
+by(bike_buyers$Income, bike_buyers$Education, summary)
+by(bike_buyers$Income, bike_buyers$Education, mean)
+by(bike_buyers$Income, bike_buyers$Education, median)
+
+boxplot(bike_buyers$Income~bike_buyers$Education, notch=TRUE,col=c("grey","gold","grey","grey","grey"), main="Income distribution among education levels")
 
 
-
-## compare educations to income
 library(sm)
 sm.density.compare(bike_buyers$Income, bike_buyers$Education, xlab="Income")
 ?sm.density.compare
@@ -80,6 +116,63 @@ colfill<-c(2:(2+length(levels(education_legend))))
 legend(locator(1), levels(education_legend), fill=colfill)
 
 
-M = cor(mtcars)
-corrplot(M, method="square")
-mtcars
+####################################### categorical & categorical
+# bought a bike vs commute distance
+?xtabs
+xtabs(~Education+Purchased.Bike, bike_buyers)
+
+
+
+
+
+
+####################################### continuous & continuous
+str(bike_buyers)
+
+# let's use a better dataset
+cars = mtcars
+scatter.smooth(cars$mpg, cars$hp, xlab="MPG", ylab="Number of Horses")
+
+
+
+
+
+####################################### what's next? Prepare for predictive modeling
+
+## tests of significance to see which variables you will want to use
+
+#### Continuous Variables Significance ####
+
+# covariance indicates positive or negative relationship between the two variables
+# this is hard to compare if the two variables have different metrics
+cov(cars$mpg, cars$hp)
+
+# Pearson's Correlation (or just correlation), standardizes the metric for both variables by using standard deviations
+# will always range from -1 to 1
+# pearson, used for LINEAR tests
+# spearmans & kendall, non-linear tolerant but not the best for non-linear relationships
+cor.test(cars$mpg, cars$hp, method="pearson")
+cor.test(cars$mpg, cars$hp, method="spearman")
+
+library(ggplot2)
+qplot(cars$mpg, cars$hp, data=mtcars, geom=c("point", "smooth"))
+?qplot
+
+
+
+#### Categorical Variables Significance ####
+
+# testing a single variable against another single variable
+purchase_to_edu = table(bike_buyers$Purchased.Bike, bike_buyers$Education)
+chisq.test(purchase_to_edu)
+?chisq.test
+
+
+
+
+
+
+
+
+
+
