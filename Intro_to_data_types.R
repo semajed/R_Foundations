e3d3a3a65af923a4a67c5410ac5497ca78751dba
James <dayhuffj@gmail.com>
2017-03-07 14:14:28 -0700

    changes from class

--- Intro_to_data_types.R
+++ Intro_to_data_types.R
@@ -1,12 +1,17 @@
+
+
 ## five most used data types
 
-my_char = "hey there"
+my_char = "hey there this class will be amazing"
 
 my_num = 3.4
 
-my_int = 300
+my_int = as.integer(300)
+my_int
 
 my_logic = FALSE
+true = TRUE
 
 
-REGIONS = factor(c("North America", "South America", "Europe", "Asia"))
+REGIONS = as.factor(c("North America", "South America", "Europe", "Asia"))
+print(REGIONS)
